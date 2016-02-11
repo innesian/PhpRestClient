f0ea6b55c0bf1f708c1be2d32c5c9e9d535a3893
Adam Innes <hook_awesome@2802921.no-reply.drupal.org>
2016-02-11 07:09:15 -0500

    Initial Commit

---
+++ src/Http.php
@@ -0,0 +1,92 @@
+<?php namespace PhpRest;
+
+trait Http
+{
+    public $default_curlopts = array(
+        'CURLOPT_CONNECTTIMEOUT' => 5,
+        'CURLOPT_RETURNTRANSFER' => true,
+        'CURLOPT_AUTOREFERER'    => true,
+        'CURLOPT_FOLLOWLOCATION' => true,
+        'CURLOPT_SSL_VERIFYPEER' => false,
+    );
+
+    public $curl_headers = array();
+
+    public function set_default_curlopts(array $options)
+    {
+        array_merge($this->default_curlopts, $options);
+
+        // Remove default options set to null.
+        foreach ($this->default_curlopts as $option=>$value) {
+            if (null == $value) {
+                unset($this->default_curlopts[$option]);
+            }
+        }
+    }
+
+    // set an associative array of headers
+    public function set_headers($headers)
+    {
+        foreach ($headers as $header=>$value) {
+            $this->curl_headers[] = "{$header}: {$value}";
+        }
+    }
+
+    public function http_request($url, $options=array())
+    {
+        // Validate URL.
+        if (filter_var($url, FILTER_VALIDATE_URL)) {
+            $options['CURLOPT_URL'] = $url;
+        } else return false;
+
+        $ch = curl_init();
+
+        // Default to use cookies.
+        if (!$options['NO_COOKIES']) {
+            $this->cookie_file = $options['CURLOPT_COOKIEFILE'] ?: false;
+            // If no cookie file passed in, create a cookie in the temp directory.
+            $this->cookie_file = $this->cookie_file  ?: tempnam(sys_get_temp_dir(), 'Http_Cookie_');
+            if (is_writeable($this->cookie_file)) {
+                $options['CURLOPT_COOKIEFILE'] = $this->cookie_file;
+                $options['CURLOPT_COOKIEJAR']  = $this->cookie_file;
+            }
+        }
+
+        // Set custom headers.
+        if (!empty($this->curl_headers)) {
+            $options['CURLOPT_HTTPHEADER'] = $this->curl_headers;
+        }
+
+        // Create array of cURL options.
+        foreach ($options as $option=>$value) {
+            if (strpos($option, 'CURLOPT_') !== false) {
+                $curlopts[constant($option)] = $value;
+            }
+        }
+
+        // Set default values not passed in.
+        foreach ($this->default_curlopts as $option=>$value) {
+            if (!isset($curlopts[$option])) {
+                $curlopts[$option] = $value;
+            }
+        }
+
+        curl_setopt_array($ch, $curlopts);
+        $this->curl_response = curl_exec($ch);
+
+        // Store request.
+        $this->curl_info = curl_getinfo($ch);
+
+        // Store error.
+        if (false === $this->curl_repsponse) {
+            $this->curl_error = curl_error($ch);
+        } else $this->curl_error = false;
+
+        curl_close($ch);
+
+        // Reset headers.
+        $this->curl_headers = array();
+
+        return $this->curl_response;
+    }
+}
---
+++ src/PhpRestClient.php
@@ -0,0 +1,114 @@
+<?php namespace PhpRestClient;
+
+class PhpRestClient
+{
+    use Http;
+
+    public $return_json_as_json = true;
+
+    /**
+     * Sets the authentication method for requests.
+     *
+     * @param string $username - Username
+     * @param string $password - Password
+     * @param int $auth - Authentication Method (CURLAUTH_BASIC or CURLAUTH_DIGEST
+     */
+    public function set_authentication($username, $password, $auth=CURLAUTH_BASIC)
+    {
+        $this->default_curlopts['CURLOPT_HTTPAUTH'] = $auth;
+        $this->default_curlopts['CURLOPT_USERPWD']  = "{$username}:{$password}";
+    }
+
+    /**
+     * Determine if the response is XML or JSON and returns parsed response.
+     */
+    public function parse_response($response) {
+        $first_char = substr(trim($response), 0, 1);
+
+        switch ($first_char) {
+            case '<': // XML.
+                $response = simplexml_load_string($response);
+                if ($response === false) {} // todo: Error.              
+                break;
+
+            case '[': case '{': // JSON.
+                $response = json_decode($response, $this->return_json_as_array);
+                if ($response === false) {} // todo: Error.
+                break;
+            default: break;
+        }
+        
+        // return parsed results or response string if neither JSON or XML.
+        return $response;
+    }
+
+    // get
+    public function get($url, $query, $headers)
+    {
+        $query = is_array($query) ? http_build_query($query) : $query;
+        $this->set_headers($headers);
+
+        $response = $this->http_request($url);
+        
+        return $this->parse_response($response);
+    }
+
+    // put
+    public function put($url, $query, $headers)
+    {
+        if (is_string($query)) {
+            $this->curl_headers[] = 'Content-Length: ' . strlen($query);
+        }
+        $options['CURLOPT_CUSTOMREQUEST'] = 'PUT';        
+        $options['CURLOPT_POSTFIELDS'] = $query;
+
+        $response = $this->http_request($url, $options);
+
+        return $this->parse_response($response);
+    }
+
+    // post
+    public function post($url, $query, $headers)
+    {
+        if (is_string($query)) {
+            $this->curl_headers[] = 'Content-Length: ' . strlen($query);
+        }
+
+        $options['CURLOPT_CUSTOMREQUEST'] = 'POST';
+        $options['CURLOPT_POST'] = true;
+        $options['CURLOPT_POSTFIELDS'] = $query;
+
+        $this->set_headers($headers);
+
+        $response = $this->http_request($url, $options);
+
+        return $this->parse_response($response);
+    }
+
+    // delete
+    public function delete($url, $headers)
+    {
+        $options['CURLOPT_CUSTOMREQUEST'] = 'DELETE';
+
+        $this->set_headers($headers);
+
+        $response = $this->http_request($url);
+
+        return $this->parse_response($response);
+    }
+
+    // patch
+    public function patch($url, $query, $headers)
+    {
+        $this->curl_headers[] = 'Content-Length: ' . strlen($query);
+
+        $options['CURLOPT_CUSTOMREQUEST'] = 'PATCH';
+        $options['CURLOPT_POSTFIELDS'] = $query;
+        
+        $this->set_headers($headers);
+
+        $response = $this->http_request($url, $options);
+
+        return $this->parse_response($response);
+    }
+}
